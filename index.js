50db2d4bfedc80f4f6054f74c5b56f10b0ff30b8
EmilbekDzhumakeev <emilbek.dzhumakeev17@gmail.com>
2022-02-07 22:48:28 -0500

    updated dependansies, changed port number in index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use('/api/users', users);
 app.use('/api/tours', tours);
 app.use('/api/bookedTours', bookedTours);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 app.listen(port, () => {
  console.log(`Server started on port: ${port}`);
 });
